cdce96a8a787e7ce95addf43686541044adc5ba5
Syed Hakeem Abbas <hakeemsyd@gmail.com>
2017-08-03 01:31:27 -0700

    First commit

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,21 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex];
+        let total = bill + tip;
+        
+        tipLabel.text = String.init(format: "$%.2f", tip);
+        totalLabel.text = String.init(format: "$%.2f", total);
+        
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello");
+        view.endEditing(true);
+    }
 }
 
